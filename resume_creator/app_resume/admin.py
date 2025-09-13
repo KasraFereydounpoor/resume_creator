215d4166454ec5d69b7053ce3c86c97319cacbef
KasraFnp <fnpkasra@gmail.com>
2025-09-13 16:16:40 +0330

    field list refactor
    admin.py field list
    to show fieldsets better and more relatable

--- resume_creator/app_resume/admin.py
+++ resume_creator/app_resume/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from .models import Profile, Experience, Skill, Education
+from django.utils.translation import ugettext_lazy as _
+
+
 
 User = get_user_model()
 
@@ -25,12 +28,26 @@ class ExperienceInLine(admin.TabularInline):
 class UserAdmin(DjangoUserAdmin):
     inlines = [ProfileInLine , EducationInLine , ExperienceInLine , SkillInLine]
     
+    def get_fieldsets(self, request, obj = None):
+        fieldsets = super().get_fieldsets(request, obj)
+        normal_user_fieldset = (
+            (None , {"fields":("username" , "password")}),
+            (_("Personal info"), {"fields": (("first_name", "last_name"), "email")}),
+        )
+        return fieldsets if request.user.is_superuser else normal_user_fieldset
 
+
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs if request.user.is_superuser else qs.filter(id=request.user.id)
+
+# Unregister the default User model admin and register our custom UserAdmin
 
 admin.site.unregister(User)
 admin.site.register(User , UserAdmin)
 
-admin.site.register(Profile)
-admin.site.register(Experience)
-admin.site.register(Skill)
-admin.site.register(Education)
+# admin.site.register(Profile)
+# admin.site.register(Experience)
+# admin.site.register(Skill)
+# admin.site.register(Education)
