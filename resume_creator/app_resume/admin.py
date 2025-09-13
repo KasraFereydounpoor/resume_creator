6b25aa3b0d82fbc58388dcd8127902fcec4d005e
KasraFnp <fnpkasra@gmail.com>
2025-09-13 10:33:43 +0330

    minor changes

--- resume_creator/app_resume/admin.py
+++ resume_creator/app_resume/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from .models import Profile, Experience, Skill, Education
 
+User = get_user_model()
 
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+
+
+class SkillInLine(admin.TabularInline):
+    model = Skill
+    extra = 0
+    
+class EducationInLine(admin.TabularInline):
+    model = Education
+    extra = 0
+    
+class ExperienceInLine(admin.TabularInline):
+    model = Experience
+    extra = 0
+class UserAdmin(DjangoUserAdmin):
+    inlines = [ProfileInLine , EducationInLine , ExperienceInLine , SkillInLine]
+    
+
+
+admin.site.unregister(User)
+admin.site.register(User , UserAdmin)
 
 admin.site.register(Profile)
 admin.site.register(Experience)
