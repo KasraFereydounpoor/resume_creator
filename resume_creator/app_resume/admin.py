031b01c6c7204a4d40685a4539e515d93c7fa6ae
KasraFnp <fnpkasra@gmail.com>
2025-09-25 16:59:50 +0330

    Refactor admin inlines and formatting in admin.py
    Renamed inline classes to use 'Inline' suffix and switched all to StackedInline for consistency. Improved import formatting and code style, and updated UserAdmin to use new inline class names. Cleaned up comments and whitespace for better readability.

--- resume_creator/app_resume/admin.py
+++ resume_creator/app_resume/admin.py
@@ -1,53 +1,57 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from .models import Profile, Experience, Skill, Education
 from django.utils.translation import gettext_lazy as _
-
+from app_resume.models import (
+    Profile,
+    Experience,
+    Skill,
+    Education,
+)
 
 
 User = get_user_model()
 
 
-
-class ProfileInLine(admin.StackedInline):
+class ProfileInline(admin.StackedInline):
     model = Profile
 
-
-class SkillInLine(admin.TabularInline):
+class SkillInline(admin.StackedInline):
     model = Skill
     extra = 0
-    
-class EducationInLine(admin.TabularInline):
+
+
+class EducationInline(admin.StackedInline):
     model = Education
     extra = 0
-    
-class ExperienceInLine(admin.TabularInline):
+
+
+class ExperienceInline(admin.StackedInline):
     model = Experience
     extra = 0
+
+
 class UserAdmin(DjangoUserAdmin):
-    inlines = [ProfileInLine , EducationInLine , ExperienceInLine , SkillInLine]
+    inlines = [ProfileInline, EducationInline, ExperienceInline, SkillInline]
     
-    def get_fieldsets(self, request, obj = None):
+    def get_fieldsets(self, request, obj=None):
         fieldsets = super().get_fieldsets(request, obj)
         normal_user_fieldset = (
-            (None , {"fields":("username" , "password")}),
-            (_("Personal info"), {"fields": (("first_name", "last_name"), "email")}),
+            (None, {"fields": ("username", "password")}),
+            (_("Personal info"), {"fields": (
+                ("first_name", "last_name"),
+            "email")}),
         )
         return fieldsets if request.user.is_superuser else normal_user_fieldset
-
-
+    
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs if request.user.is_superuser else qs.filter(id=request.user.id)
 
-# Unregister the default User model admin and register our custom UserAdmin
+
 
 admin.site.unregister(User)
-admin.site.register(User , UserAdmin)
+admin.site.register(User, UserAdmin)
 
-# admin.site.register(Profile)
-# admin.site.register(Experience)
-# admin.site.register(Skill)
-# admin.site.register(Education)
+
